3ff4169e5fc340ff737f2b52d75a57b01d7a1d2f
Jwallack7 <98927166+Jwallack7@users.noreply.github.com>
2022-04-14 10:11:00 -0400

    Add files via upload

---
+++ pythonQueue.py
@@ -0,0 +1,19 @@
+#Jason
+#PythonQueueAssignment
+names=[ ]
+for x in range(10):
+    name= input ("Enter Names:")
+    names.append(name)
+
+print(names)
+
+for x in range(10):
+    print(names.pop(0))
+
+print(names)
+
+    
+
+    
+    
+    
